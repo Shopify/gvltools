4f36df77be874b01f6a1fd6ff2eb84f45071208e
Jean Boussier <jean.boussier@gmail.com>
2022-06-18 09:41:42 +0200

    Remove std=c99
    Otherwise gcc won't let us use `clock_gettime` and `_Thread_local`.

--- ext/gvltools/extconf.rb
+++ ext/gvltools/extconf.rb
@@ -2,7 +2,7 @@
 
 require "mkmf"
 if RUBY_ENGINE == "ruby" && have_func("rb_internal_thread_add_event_hook", ["ruby/thread.h"])
-  $CFLAGS << " -O3 -Wall -std=c99 "
+  $CFLAGS << " -O3 -Wall "
   create_makefile("gvltools/instrumentation")
 else
   File.write("Makefile", dummy_makefile($srcdir).join)
