7d78db0594bc18dd616f62bd5a8ac0853708d9e7
Jean Boussier <jean.boussier@gmail.com>
2023-02-27 12:53:03 +0100

    Replace rb_atomic_t by C11 atomics
    Fix: https://github.com/Shopify/gvltools/pull/3
    
    `rb_atomic_t` is `unsigned int` (32bits) so can wrap around
    too quickly.
    
    This requires more modern compilers but I don't think
    that is an issue.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 ## [Unreleased]
 
+- Use C11 atomics instead of MRI's `rb_atomic_t`.
+
 ## [0.1.0] - 2022-06-14
 
 - Initial release
--- ext/gvltools/extconf.rb
+++ ext/gvltools/extconf.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 require "mkmf"
-if RUBY_ENGINE == "ruby" && have_func("rb_internal_thread_add_event_hook", ["ruby/thread.h"])
+if RUBY_ENGINE == "ruby" &&
+   have_header("stdatomic.h") &&
+   have_func("rb_internal_thread_add_event_hook", ["ruby/thread.h"])
+
   $CFLAGS << " -O3 -Wall "
   create_makefile("gvltools/instrumentation")
 else
